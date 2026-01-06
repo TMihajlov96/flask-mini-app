bba4673bbcfec816be733c5aa8be7cafe379354f
Tea Mihajlov <teodoramihajlov@gmail.com>
2026-01-06 17:32:22 +0100

    Fix .run. Add host argument.

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-import os
 from flask import Flask
-
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return "You're home now!"
+
 @app.route("/hello-world")
 def hello_world():
     return "Hello World"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(host="0.0.0.0", port=8080)
