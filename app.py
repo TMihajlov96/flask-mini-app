206c24e8308863e7f0616b324525811ca7afcb17
Tea Mihajlov <teodoramihajlov@gmail.com>
2026-01-06 17:37:29 +0100

    Fix return message for '/hello-world' path.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def main():
 
 @app.route("/hello-world")
 def hello_world():
-    return "Hello World"
+    return "Hello World!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
